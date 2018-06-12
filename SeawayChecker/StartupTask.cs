251da376fdd5ce5449247b23503a44e1d1d4e52b
Christophe Asselin <christophe.asselin@hotmail.com>
2018-06-12 14:44:42 -0400

    Initial Commit

--- SeawayChecker/StartupTask.cs
+++ SeawayChecker/StartupTask.cs
@@ -2,24 +2,77 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Net;
 using System.Net.Http;
+using System.IO;
 using Windows.ApplicationModel.Background;
+using Windows.Devices.Gpio;
+using Windows.System.Threading;
+using System.Threading;
 
-// The Background Application template is documented at http://go.microsoft.com/fwlink/?LinkID=533884&clcid=0x409
 
 namespace SeawayChecker
 {
     public sealed class StartupTask : IBackgroundTask
     {
+        BackgroundTaskDeferral deferral;
+        private GpioPinValue value = GpioPinValue.High;
+        private const int GREEN_LED_PIN = 3;
+        private const int RED_LED_PIN = 5;
+        private GpioPin greenPin;
+        private GpioPin redPin;
+        private ThreadPoolTimer timer;
+        private bool isOpened;
+
         public void Run(IBackgroundTaskInstance taskInstance)
         {
-            // 
-            // TODO: Insert code to perform background work
-            //
-            // If you start any asynchronous methods here, prevent the task
-            // from closing prematurely by using BackgroundTaskDeferral as
-            // described in http://aka.ms/backgroundtaskdeferral
-            //
+            deferral = taskInstance.GetDeferral();
+            InitGPIO();
+
+            timer = ThreadPoolTimer.CreatePeriodicTimer(GetStatus, TimeSpan.FromMinutes(10));
+        }
+
+        private void InitGPIO()
+        {
+            greenPin = GpioController.GetDefault().OpenPin(GREEN_LED_PIN);
+            greenPin.Write(GpioPinValue.High);
+            greenPin.SetDriveMode(GpioPinDriveMode.Output);
+
+            redPin = GpioController.GetDefault().OpenPin(RED_LED_PIN);
+            redPin.Write(GpioPinValue.Low);
+            redPin.SetDriveMode(GpioPinDriveMode.Output);
+        }
+
+        private async void GetStatus(ThreadPoolTimer timer)
+        {
+            HttpClient client = new HttpClient();
+            string url = "http://www.greatlakes-seaway.com/R2/jsp/mMaiBrdgStatus.jsp?language=E";
+            HttpResponseMessage response = await client.GetAsync(url);
+            string html = response.Content.ToString();
+            var htmlDoc = new HtmlAgilityPack.HtmlDocument();
+            htmlDoc.LoadHtml(html);
+            var htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/ul/li[2]/span[3]/font");
+            if (htmlNode.InnerText.Trim() == "Available")
+                isOpened = true;
+            else
+                isOpened = false;
+            UpdateLEDs();
+        }
+
+        private void UpdateLEDs()
+        {
+            if (isOpened)
+            {
+                greenPin.Write(GpioPinValue.High);
+                redPin.Write(GpioPinValue.Low);
+            }
+            else
+            {
+                redPin.Write(GpioPinValue.High);
+                greenPin.Write(GpioPinValue.Low);
+            }
+                
         }
     }
 }
+
