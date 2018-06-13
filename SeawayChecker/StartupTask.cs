e9786451560170c2d289f56fb259e05384af6dcd
Christophe Asselin <christophe.asselin@hotmail.com>
2018-06-13 19:17:21 -0400

    Removed useless dependencies

--- SeawayChecker/StartupTask.cs
+++ SeawayChecker/StartupTask.cs
@@ -1,14 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Net;
 using System.Net.Http;
-using System.IO;
 using Windows.ApplicationModel.Background;
 using Windows.Devices.Gpio;
 using Windows.System.Threading;
-using System.Threading;
 
 
 namespace SeawayChecker
