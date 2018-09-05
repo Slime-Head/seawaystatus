5f93847eb17fdf40c721b13d10486d1b37284567
Christophe Asselin <christophe.asselin@hotmail.com>
2018-09-05 13:58:55 -0400

    Added method and class documentation.

--- SeawayChecker/StartupTask.cs
+++ SeawayChecker/StartupTask.cs
@@ -7,6 +7,9 @@ using Windows.System.Threading;
 
 namespace SeawayChecker
 {
+    /// <summary>
+    /// UWP app background task
+    /// </summary>
     public sealed class StartupTask : IBackgroundTask
     {
         BackgroundTaskDeferral deferral;
@@ -19,8 +22,13 @@ namespace SeawayChecker
         private ThreadPoolTimer timer;
 
         private const string url = "http://www.greatlakes-seaway.com/R2/jsp/mMaiBrdgStatus.jsp?language=E";
+        private const string availabilityNode = "/html/body/div/div[2]/ul/li[2]/span[3]/font";
         private bool isOpen;
 
+        /// <summary>
+        /// Runs the main app
+        /// </summary>
+        /// <param name="taskInstance"></param>
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
@@ -29,6 +37,9 @@ namespace SeawayChecker
             timer = ThreadPoolTimer.CreatePeriodicTimer(GetStatus, TimeSpan.FromMinutes(1));
         }
 
+        /// <summary>
+        /// Initializes the GPIO pins for the LEDs
+        /// </summary>
         private void InitGPIO()
         {
             greenPin = GpioController.GetDefault().OpenPin(GREEN_LED_PIN);
@@ -40,15 +51,20 @@ namespace SeawayChecker
             redPin.SetDriveMode(GpioPinDriveMode.Output);
         }
 
+        /// <summary>
+        /// Gets html from the website and parses it to get the bridge status
+        /// </summary>
+        /// <param name="timer"></param>
         private async void GetStatus(ThreadPoolTimer timer)
         {
             HttpClient client = new HttpClient();
             string html = await client.GetStringAsync(url);
-            html = html.Substring(622); // Remove text before start of html node
+            int nCharactersToRemove = 622; // Text to remove before start of html node
+            html = html.Substring(nCharactersToRemove);
 
             var htmlDoc = new HtmlAgilityPack.HtmlDocument();
             htmlDoc.LoadHtml(html);
-            var htmlNode = htmlDoc.DocumentNode.SelectSingleNode("/html/body/div/div[2]/ul/li[2]/span[3]/font");
+            var htmlNode = htmlDoc.DocumentNode.SelectSingleNode(availabilityNode); 
             string availability = htmlNode.InnerText.Trim();
 
             if (availability == "Available")
@@ -59,17 +75,23 @@ namespace SeawayChecker
             UpdateLEDs();
         }
 
+        /// <summary>
+        /// Updates the LEDs according to the bridge status.
+        /// </summary>
         private void UpdateLEDs()
         {
+            GpioPinValue on = GpioPinValue.Low;
+            GpioPinValue off = GpioPinValue.High;
+
             if (isOpen)
             {
-                greenPin.Write(GpioPinValue.Low); // Green ON
-                redPin.Write(GpioPinValue.High); // Red OFF
+                greenPin.Write(on); // Green ON
+                redPin.Write(off); // Red OFF
             }
             else
             {
-                redPin.Write(GpioPinValue.Low); // Green ON
-                greenPin.Write(GpioPinValue.High); // Red OFF
+                redPin.Write(on); // Green ON
+                greenPin.Write(off); // Red OFF
             }   
         }
     }
